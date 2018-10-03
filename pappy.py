ac023e733bf91f18c115686e0d9ba65a6c5dd944
Tim Ottinger <tottinge@industriallogic.com>
2018-10-03 11:31:07 -0400

    Adds a rget to the auth_session

--- pappy.py
+++ pappy.py
@@ -1,16 +1,19 @@
 from requests import get, post, put, delete, patch, Session
 from box import Box
 
-def auth_session(auth=None):
+def auth_session(auth=None, base=None):
+    import types
+    def rget(s, relative, **vargs):
+        return s.get(f'{s.base}/{relative}', **vargs)
     session = Session()
     if auth:
         session.headers['Authorization'] = f'Bearer {auth}'
+    session.base = base or ''
+    session.rget = types.MethodType(rget, session)
     return session
-
 
 def body_for(request):
     return Box(request.json())
 
 "Should run with ipython -i for interactive usage"
 
-print("whoo: this is ready to rock")
